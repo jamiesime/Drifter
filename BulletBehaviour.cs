5cb8bffd60cebab7f7a330a3a7552a6a018d7361
GHOST-HUB\Jamie <jamiesime@gmail.com>
2018-02-20 10:01:57 +0000

    added basic firing component scripts, Weapons and Bullets

---
+++ BulletBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+
+	public float destroyTime;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		destroyTime += Time.deltaTime;
+		transform.Translate(Vector3.forward, Space.Self);
+		if(destroyTime > 1.5f){
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Weapons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour {
+
+	public GameObject smallCannon;
+	public GameObject smallShell;
+	public float firePeriod;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// gets right fire and handles firing and animations
+		handleSmallCannon();
+	}
+
+	public void handleSmallCannon(){
+		bool rightFire = Input.GetButton("RightFire");
+		Vector3 rightAmmoSpawnPos = new Vector3(smallCannon.transform.position.x, smallCannon.transform.position.y, smallCannon.transform.position.z);
+
+		if(rightFire){
+
+			if(firePeriod == 0f){
+				GameObject newBullet = Instantiate(smallShell, rightAmmoSpawnPos, transform.rotation);
+				newBullet.transform.parent = smallCannon.transform;
+			}
+			
+			firePeriod += Time.deltaTime;
+
+			if(firePeriod > 0.1f){
+				firePeriod = 0f;
+			}
+		}
+		else{
+			firePeriod = 0f;
+		}
+	}
+}
