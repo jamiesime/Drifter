2b347f5d2ac6c199bdb858ee174a0d68bee28c5c
GHOST-HUB\Jamie <jamiesime@gmail.com>
2018-02-21 09:15:13 +0000

    added simple movement script for asteroids

---
+++ AsteroidMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMovement : MonoBehaviour {
+
+	public GameObject target;
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = Random.Range(0.5f, 3f);
+		if(target != null){
+			this.gameObject.transform.LookAt(target.transform);
+		}
+		else{
+			transform.rotation = Random.rotation;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(target != null){
+			this.gameObject.transform.LookAt(target.transform);
+		}
+		transform.Translate(Vector3.forward * (Time.deltaTime * moveSpeed));
+	}
+}
